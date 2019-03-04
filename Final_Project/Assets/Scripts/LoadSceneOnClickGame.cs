d15f92c295e8c19569286123ff55a702b4d34ec9
Samuel <ssitero@knights.ucf.edu>
2019-03-03 21:19:37 -0500

    MainMenu protoype in progress

---
+++ Final_Project/Assets/Scripts/LoadSceneOnClickGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClickGame : MonoBehaviour
+{
+    public void LoadByIndex(int buildIndex)
+    {
+
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Final_Project/Assets/Scripts/LoadSceneOnClickTut.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClickTut : MonoBehaviour
+{
+    public void LoadByIndex(int buildIndex)
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
