dede2ba17809985b7b395acc11516e26b0c66532
Samuel <ssitero@knights.ucf.edu>
2019-03-13 00:13:48 -0400

    grass added
    Detail distance being worked on

---
+++ Final_Project/Assets/Scripts/DrawDistance.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawDistance : MonoBehaviour
+{
+
+    public Terrain m_terrain;  //reference to your terrain
+    public float DrawDistanceNum; // how far you want to be able to see the grass
+
+    // Use this for initialization
+    void Start()
+    {
+
+        m_terrain.detailObjectDistance = DrawDistanceNum;
+
+    }
+}
