571d266e1e289a7a0738270a621bfbb1f5908841
dwiddel <david.widdel@gmail.com>
2019-03-18 23:00:28 -0400

    More functionality

---
+++ Final_Project/Assets/Scripts/CanvasControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CanvasControl : MonoBehaviour
+{
+    GameObject levelScreen;
+
+    void Start()
+    {
+        //levelScreen = GameObject.Find("LevelScreen");
+        //Time.timeScale = 0f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            //levelScreen.SetActive(false);
+            //Time.timeScale = 1f;
+        }
+    }
+
+    public void LoadByIndex(int buildIndex)
+    {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+    }
+
+    public void LoadTutorial()
+    {
+
+    }
+
+    public void ShowLevelScreen()
+    {
+
+    }
+
+    public void HideLevelScreen()
+    {
+        //levelScreen.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
--- Final_Project/Assets/Scripts/GameStatus.cs
+++ Final_Project/Assets/Scripts/GameStatus.cs
@@ -7,52 +7,38 @@ using UnityEngine.SceneManagement;
 
 public class GameStatus : MonoBehaviour
 {
-    //There are 3 major ways to persist this data between scenes
-    // 1) Save the info into something persist (UserPrefs, a save file)
-    //      - This preserves data even between game executions, not just scene changes
-    //
-    // 2) Static class data. This is Probably not the prefence for a beginner.
-    // 3) DontDestroyOnLoad -- This flags a GameObject such that when we cahnge from one scene to another, it doesn't get destroyed. (i.e. it is still present in the newly loaded scene. To use this to the best possible advantage, you need to use the Unity Singleton Design Pattern.
 
+    Scene m_Scene;
+
+    [HideInInspector]
+    private int finishCurrentSceneCount;
     public int wood = 0;
     public int score = 0;
     public int highScore = 0;
-    Scene m_Scene;
     public string MainMenu;
+
+    [Header ("Total collectable wood:")]
+    public int woodCount;
+ 
 
     // Start is called before the first frame update
     void Start()
     {
-        // Implementation #1
-        // Load data from PlayerPrefs -- this might be from the previous
-        // scene, or maybe from the previous execution
         score = PlayerPrefs.GetInt("score", 0);
         wood = PlayerPrefs.GetInt("wood", 0);
         highScore = PlayerPrefs.GetInt("highScore", 0);
+        GetCurrentScene();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Return the current Active Scene in order to get the current Scene's name
-        m_Scene = SceneManager.GetActiveScene();
-        //
-        if (m_Scene.name == MainMenu)
-        {
-            Debug.Log("On the main menu.");
-        }
+
     }
 
     private void OnDestroy()
     {
         Debug.Log("GameStatus was destroyed.");
-        // Before we get destroyed, let's save our data to our save file.
-        // This is "Implementation #1".
-        // This will happen whenever this object is destroyed, which
-        // includes scene changes as well as simply exiting the program.
-        // string saveSlot = "saveSlot" + mySaveNumber;
-        // PlayerPrefs.SetString("saveSlot0_inv0_logs", 10);
-
         PlayerPrefs.SetInt("score", score);
         PlayerPrefs.SetInt("wood", wood);
         PlayerPrefs.SetInt("highScore", highScore);
@@ -63,22 +49,59 @@ public class GameStatus : MonoBehaviour
         score += s;
     }
 
+    public void AddWood(int w)
+    {
+        wood += w;
+        finishCurrentSceneCount = finishCurrentSceneCount + 1;
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        //countText.text = "Count: " + finishCurrentSceneCount.ToString();
+        if (finishCurrentSceneCount >= woodCount)
+        {
+            //winText.text = "You Win!";
+            ResetWood();
+            Debug.Log("All wood has been picked up.");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            //SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        }
+    }
+
     // Resets PlayerPrefs to default when game is quit
     public void ResetScore()
     {
         score = 0;
+    }
+
+    // Resets PlayerPrefs to default when game is quit
+    public void ResetWood()
+    {
         wood = 0;
+    }
+
+    // Resets PlayerPrefs to default when game is quit
+    public void ResetAll()
+    {
+        wood = 0;
+        score = 0;
+    }
+
+    // Resets PlayerPrefs to default when game is quit
+    public void ResetHighScore()
+    {
+        highScore = 0;
     }
 
     public void GetCurrentScene()
     {
-        //Debug.Log("On the main menu.");
         //Return the current Active Scene in order to get the current Scene's name
         m_Scene = SceneManager.GetActiveScene();
         //
         if (m_Scene.name == MainMenu)
         {
-            Debug.Log("On the main menu.");
+            Debug.Log("Current Scene: " + m_Scene.name);
         }
     }
 
