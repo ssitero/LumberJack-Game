eb40d5221f41f04f7f498ce9cc04680452afd253
Samuel <ssitero@knights.ucf.edu>
2019-03-21 20:39:26 -0400

    Prototype tree chopping on collision

---
+++ Final_Project/Assets/Scripts/TreeBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeBehavior : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        
+         if (other.gameObject.CompareTag("Player"))
+        {
+           
+
+           // if (this.gameObject.tag == "Tree")
+           // {
+                this.gameObject.SetActive(false);
+            }
+            
+        }
+}
