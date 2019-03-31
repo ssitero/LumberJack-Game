1b945cc79c085c544fb5432c4fb3919773bd3c99
Samuel <ssitero@knights.ucf.edu>
2019-03-31 01:14:39 -0400

    Tree Chopping in progress

--- Final_Project/Assets/Scripts/TreeBehavior.cs
+++ Final_Project/Assets/Scripts/TreeBehavior.cs
@@ -4,17 +4,99 @@ using UnityEngine;
 
 public class TreeBehavior : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
-    {
-        
-         if (other.gameObject.CompareTag("Player"))
-        {
-           
 
-           // if (this.gameObject.tag == "Tree")
-           // {
-                this.gameObject.SetActive(false);
-            }
-            
+    public float cubeSize = 0.2f;
+    public int cubesInRow = 5;
+
+    float cubesPivotDistance;
+    Vector3 cubesPivot;
+
+    public float explosionForce = 50f;
+    public float explosionRadius = 4f;
+    public float explosionUpward = 0.4f;
+    public float lifetime = 3.0f;
+    public Material wood;
+
+
+
+    void Start()
+    {
+
+
+
+        cubesPivotDistance = cubeSize * cubesInRow / 2;
+
+        cubesPivot = new Vector3(cubesPivotDistance, cubesPivotDistance, cubesPivotDistance);
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            explode();
         }
+
+    }
+
+    public void explode()
+    {
+
+        this.gameObject.SetActive(false);
+
+
+        for (int x = 0; x < cubesInRow; x++)
+        {
+            for (int y = 0; y < cubesInRow; y++)
+            {
+                for (int z = 0; z < cubesInRow; z++)
+                {
+                    createPiece(x, y, z);
+                }
+            }
+        }
+
+
+        Vector3 explosionPos = transform.position;
+
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+
+        foreach (Collider hit in colliders)
+        {
+
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius, explosionUpward);
+            }
+        }
+
+    }
+
+    void createPiece(int x, int y, int z)
+    {
+
+
+        GameObject piece;
+        piece = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        piece.GetComponent<Renderer>().material = wood;
+
+
+        piece.transform.position = transform.position + new Vector3(cubeSize * x, cubeSize * y, cubeSize * z) - cubesPivot;
+        piece.transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
+
+       
+        piece.AddComponent<Rigidbody>();
+        piece.GetComponent<Rigidbody>().mass = cubeSize;
+        Destroy(piece, lifetime);
+
+    }
+
 }
