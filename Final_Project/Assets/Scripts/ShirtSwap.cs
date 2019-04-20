80cf0e11f0c766e72a4e1c8a49d48995a189288a
Samuel <ssitero@knights.ucf.edu>
2019-04-20 00:27:52 -0400

    shirt swap in progress

---
+++ Final_Project/Assets/Scripts/ShirtSwap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShirtSwap : MonoBehaviour
+{
+    //public Material Blue;
+    public Material[] material;
+    Renderer rend;
+   
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+   
+     void OnCollisionEnter(Collision col){
+
+        /*if (col.gameObject.CompareTag("Shirt")){
+           
+            this.gameObject.GetComponent<Renderer>().material = Blue;
+            */
+        if(col.gameObject.tag == "Shirt"){
+            rend.sharedMaterial = material[1];
+        }
+
+        }
+    }
+
